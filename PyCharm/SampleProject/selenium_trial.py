beea91c1c39ecef5710e00a822b7b8eb034d7032
taksahi <takashi4233@gmail.com>
2020-02-08 22:35:41 +0900

    gitignore file

---
+++ PyCharm/SampleProject/selenium_trial.py
@@ -0,0 +1,15 @@
+import selenium
+
+
+def add(num1, num2):
+    return num1 + num2
+
+
+def main():
+    num1 = 1
+    num2 = 2
+    print(add(num1, num2))
+
+
+if __name__ == '__main__':
+    main()
